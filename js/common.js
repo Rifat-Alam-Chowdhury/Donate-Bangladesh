99cfa03d09c62f5067f6eaceb44d539fe8f71bdd
rifat chowdhury <rifatalamtabs2@gmail.com>
2024-09-25 01:41:32 +0600

    without blog all done

---
+++ js/common.js
@@ -0,0 +1,17 @@
+function id(id) {
+  const ids = parseFloat(document.getElementById(id).value);
+  return ids;
+}
+
+function texts(id) {
+  const ids = parseFloat(document.getElementById(id).innerText);
+  return ids;
+}
+
+function hidden(id) {
+  document.getElementById("drop-down").classList.add("hidden");
+  document.getElementById("history-section").classList.add("hidden");
+  document.getElementById("Donation-btn").classList.add("hidden");
+
+  document.getElementById(id).classList.remove("hidden");
+}
--- js/nav.js
+++ js/nav.js
@@ -1,15 +1,24 @@
-document.getElementById("Submit-btn").addEventListener("click", function () {
-  const userInput = parseFloat(document.getElementById("cash-in-field").value);
+const time = new Date();
 
-  const currentMainBalance = parseFloat(
-    document.getElementById("main-balance").innerText
-  );
+document.getElementById("Submit-btn").addEventListener("click", function () {
+  const userInput = id("cash-in-field");
+
+  const currentMainBalance = texts("main-balance");
 
   if (isNaN(userInput)) {
-    // alert("please input amount first");
     my_modal_1.showModal();
     return;
   }
+
+  //   history
+
+  const history = document.createElement("div");
+  history.className =
+    "card-body bg-gradient-to-t from-gray-300/50 to-red-400/90 border-4 my-1 w-80 shadow-xl  mx-auto  border-red-600 rounded-xl";
+  history.innerHTML = `<h2 class="card-title">Cash In</h2>
+            <p class=""><span class="text-red-600 font font-extrabold">${userInput}</span> BDT has been cashed in at ${time}</p>`;
+  document.getElementById("history-section").append(history);
+  //   history ends
 
   document.getElementById("main-balance").innerText =
     currentMainBalance + userInput;
@@ -20,3 +29,113 @@ document.getElementById("Submit-btn").addEventListener("click", function () {
 document.getElementById("cash-outer").addEventListener("click", function () {
   document.getElementById("drop-down").classList.remove("hidden");
 });
+
+document.getElementById("History-btn").addEventListener("click", function () {
+  document.getElementById("history-section").classList.remove("hidden");
+  document.getElementById("cards-section").classList.add("hidden");
+});
+
+document.getElementById("Donation-btn").addEventListener("click", function () {
+  document.getElementById("history-section").classList.add("hidden");
+  document.getElementById("cards-section").classList.remove("hidden");
+});
+
+// card 1
+document.getElementById("donate-btn").addEventListener("click", function () {
+  const userInput = id("donation-user-field");
+  const currentMainBalance = texts("main-balance");
+  const donationBox = texts("donation-box");
+
+  if (isNaN(userInput) || isNaN(donationBox)) {
+    my_modal_2.showModal();
+    return;
+  }
+
+  if (userInput > currentMainBalance) {
+    my_modal_3.showModal();
+    return;
+  }
+
+  //   history for donation
+  const history = document.createElement("div");
+  history.className =
+    "card-body bg-gradient-to-t from-gray-300/50 to-red-400/90 border-4 my-1 w-80 shadow-xl  mx-auto  border-red-600 rounded-xl";
+  history.innerHTML = `<h2 class="card-title">Donated in Noakhali</h2>
+            <p class=""><span class="text-red-600 font font-extrabold">${userInput}</span> BDT has been donated in Noakhali at ${time}</p>`;
+  document.getElementById("history-section").append(history);
+  //   history for donation ends
+
+  document.getElementById("donation-box").innerText = donationBox + userInput;
+  document.getElementById("main-balance").innerText =
+    currentMainBalance - userInput;
+  my_modal_4.showModal();
+});
+
+// card 1 ends
+
+// card 2
+
+document.getElementById("donate-btn2").addEventListener("click", function () {
+  const userInput = id("donation-user-field2");
+  const currentMainBalance = texts("main-balance");
+  const donationBox = texts("donation-box2");
+
+  if (isNaN(userInput) || isNaN(donationBox)) {
+    my_modal_2.showModal();
+    return;
+  }
+
+  if (userInput > currentMainBalance) {
+    my_modal_3.showModal();
+    return;
+  }
+
+  //   history for donation
+  const history = document.createElement("div");
+  history.className =
+    "card-body bg-gradient-to-t from-gray-300/50 to-red-400/90 border-4 my-1 w-80 shadow-xl  mx-auto  border-red-600 rounded-xl";
+  history.innerHTML = `<h2 class="card-title">Donated in FENI</h2>
+              <p class=""><span class="text-red-600 font font-extrabold">${userInput}</span> BDT has been donated in FENI at ${time}</p>`;
+  document.getElementById("history-section").append(history);
+  //   history for donation ends
+
+  document.getElementById("donation-box2").innerText = donationBox + userInput;
+  document.getElementById("main-balance").innerText =
+    currentMainBalance - userInput;
+
+  my_modal_4.showModal();
+});
+// card 2 ends
+
+// card 3
+document.getElementById("donate-btn3").addEventListener("click", function () {
+  const userInput = id("donation-user-field3");
+  const currentMainBalance = texts("main-balance");
+  const donationBox = texts("donation-box3");
+
+  if (isNaN(userInput) || isNaN(donationBox)) {
+    my_modal_2.showModal();
+    return;
+  }
+
+  if (userInput > currentMainBalance) {
+    my_modal_3.showModal();
+    return;
+  }
+
+  //   history for donation
+  const history = document.createElement("div");
+  history.className =
+    "card-body bg-gradient-to-t from-gray-300/50 to-red-400/90 border-4 my-1 w-80 shadow-xl  mx-auto  border-red-600 rounded-xl";
+  history.innerHTML = `<h2 class="card-title">Donated in Aid for Injured in the Quota Movement</h2>
+                <p class=""><span class="text-red-600 font font-extrabold">${userInput}</span> BDT has been donated in Quota at ${time}</p>`;
+  document.getElementById("history-section").append(history);
+  //   history for donation ends
+
+  document.getElementById("donation-box3").innerText = donationBox + userInput;
+  document.getElementById("main-balance").innerText =
+    currentMainBalance - userInput;
+
+  my_modal_4.showModal();
+});
+// card 3 ends

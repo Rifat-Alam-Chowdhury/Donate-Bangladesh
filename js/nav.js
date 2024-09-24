7df9118eb1c07fcf0117023b0e768cd16ef64d1a
rifat chowdhury <rifatalamtabs2@gmail.com>
2024-09-24 21:09:26 +0600

    nav-bar everthing has been done

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,22 @@
-document
-  .getElementById("cash-in-btn-outer")
-  .addEventListener("click", function () {
-    document.getElementById("drop-down").classList.remove("hidden");
-    const userInput = parseFloat(
-      document.getElementById("cash-in-field").value
-    );
-    const currentMainBalance = parseFloat(
-      document.getElementById("main-balance").innerText
-    );
+document.getElementById("Submit-btn").addEventListener("click", function () {
+  const userInput = parseFloat(document.getElementById("cash-in-field").value);
 
-    document.getElementById("main-balance").innerText =
-      userInput + currentMainBalance;
-  });
-document.getElementById("cash-in-btn").addEventListener("click", function () {
+  const currentMainBalance = parseFloat(
+    document.getElementById("main-balance").innerText
+  );
+
+  if (isNaN(userInput)) {
+    // alert("please input amount first");
+    my_modal_1.showModal();
+    return;
+  }
+
+  document.getElementById("main-balance").innerText =
+    currentMainBalance + userInput;
+
   document.getElementById("drop-down").classList.add("hidden");
+});
+
+document.getElementById("cash-outer").addEventListener("click", function () {
+  document.getElementById("drop-down").classList.remove("hidden");
 });
